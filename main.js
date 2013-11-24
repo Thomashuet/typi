5a2bb4486854d868f2737d386968b5d83354723e
Thomas HUET <thomas.h@ens.fr>
2013-11-24 19:23:33 +0100

    Add shortcut to reset OCaml

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ var invite = document.getElementById("prompt");
 var interpreter = new Worker("ocaml/toplevel.js");
 var ready = false;
 var lines = 1;
+invite.style.display = "none";
+input.style.display = "none";
 
 function onresponse(e) {
   if("out" in e.data)
@@ -17,6 +19,7 @@ function onresponse(e) {
     ready = true;
     lines = 1;
     input.rows = 1;
+    input.rows = lines;
     invite.style.display = "block";
     input.style.display = "block";
     input.value = "";
@@ -28,10 +31,18 @@ interpreter.onmessage = onresponse;
 function reset() {
   interpreter.terminate();
   interpreter = new Worker("ocaml/toplevel.js");
-  output.value = "";
+  output.innerHTML = "";
   ready = false;
   lines = 1;
+  invite.style.display = "none";
+  input.style.display = "none";
   interpreter.onmessage = onresponse;
+}
+
+document.onkeypress = function(ev) {
+  if(ev.keyCode == 27 && ev.ctrlKey) {
+    reset();
+  }
 }
 
 function send(s) {
@@ -53,22 +64,34 @@ function key(ev) {
   }
 }
 
+function executeAll() {
+  var sentence = /[^;]*;;/g;
+  var s = editor.getValue();
+  while((res = sentence.exec(s)) !== null) {
+    send(res[0].replace(/^\s*/, ""));
+  }
+}
+
+function execute() {
+  var sentence = /[^;]*;;/g;
+  var i = editor.getSession().getDocument().positionToIndex(editor.getCursorPosition());
+  var s = editor.getValue();
+  var cur = "";
+  while((res = sentence.exec(s)) !== null && res.index < i) {
+    cur = res[0];
+  }
+  cur = cur.replace(/^\s*/, "");
+  if(cur !== "") {
+    send(cur);
+  }
+}
+
 function submit(ev) {
   if(ev.keyCode == 13 && ev.ctrlKey) {
     if(ev.shiftKey) {
-      send(editor.getValue());
+      executeAll();
     } else {
-      var sentence = /[^;]*;;/g;
-      var i = editor.getSession().getDocument().positionToIndex(editor.getCursorPosition());
-      var s = editor.getValue();
-      var cur = "";
-      while((res = sentence.exec(s)) !== null && res.index < i) {
-	cur = res[0];
-      }
-      cur = cur.replace(/^\s*/, "");
-      if(cur !== "") {
-	send(cur);
-      }
+      execute();
     }
   }
 }
